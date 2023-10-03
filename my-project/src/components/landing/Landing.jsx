2f055053b0624d90b9550f2148869454b9557677
George Claridge <glgclaridge@gmail.com>
2023-10-03 14:38:35 +1300

    Added Landing button animation

--- my-project/src/components/landing/Landing.jsx
+++ my-project/src/components/landing/Landing.jsx
@@ -16,13 +16,16 @@ const Landing = () => {
         <div className="Header text-4xl md:text-6xl font-space">Software is Built</div>
         <div className="Subheader text-base md:text-xl py-3 font-open">Design, build, share what makes you smile</div>
 
-        <div className='flex justify-center'>
-          <img className="w-10 h-10 mt-10" src={PopLines} alt="" />
-          <a href="https://www.figma.com/file/yUf60Lk7wxET028cQ6ukCS/Untitled?type=design&node-id=0%3A1&mode=design&t=9cSjTM2GTZo9Gxq0-1" target="_blank" rel="noopener noreferrer">
-            <button className="Button py-2 px-8 bg-orange hover:bg-purple duration-300 text-lg md:text-2xl font-bold dropbox font-space self-start">Join The Community</button>
-          </a>
-          <div className="w-10 h-10 mt-10"></div>
+        <div className='flex'>
+          <div className='flex justify-center group mx-auto'>
+            <img className="w-10 h-10 mt-10 group-hover:scale-125 group-hover:translate-y-1 group-hover:-translate-x-1 duration-200" src={PopLines} alt="" />
+            <a href="https://www.figma.com/file/yUf60Lk7wxET028cQ6ukCS/Untitled?type=design&node-id=0%3A1&mode=design&t=9cSjTM2GTZo9Gxq0-1" target="_blank" rel="noopener noreferrer">
+              <button className="Button py-2 px-8 bg-orange group-hover:bg-purple duration-300 text-lg md:text-2xl font-bold dropbox font-space self-start">Join The Community</button>
+            </a>
+            <div className="w-10 h-10 mt-10"></div>
+          </div>
         </div>
+
       </div>
         
     </div>

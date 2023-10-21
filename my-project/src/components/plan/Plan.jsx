1db5625d770ed9af624d4f9f8c54a2b30a8dad64
George Claridge <glgclaridge@gmail.com>
2023-10-21 13:16:02 +1300

    Fixed Background

--- my-project/src/components/plan/Plan.jsx
+++ my-project/src/components/plan/Plan.jsx
@@ -3,7 +3,7 @@ import { BackgroundDouble } from './import';
 
 const Plan = () => {
   return (
-    <div className='grid grid-cols-1 gap-12 lg:grid-cols-3 bg-green px-4 md:px-20 xl:px-60 py-10 md:py-20 bg-no-repeat bg-center'  style={{ backgroundImage: `url(${BackgroundDouble})` }} >
+    <div className='grid grid-cols-1 gap-12 lg:grid-cols-3 bg-green px-4 md:px-20 xl:px-60 py-10 md:py-20 bg-no-repeat bg-center bg-cover'  style={{ backgroundImage: `url(${BackgroundDouble})` }} >
 
       <div className="card dropbox">
 

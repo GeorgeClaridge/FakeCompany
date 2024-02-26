c37f9100ebcef0142afa8e6ee9ab47a7c9d8287c
George Claridge <glgclaridge@gmail.com>
2024-02-27 12:26:16 +1300

    Layout Changes

--- my-project/src/components/landing/Landing.jsx
+++ my-project/src/components/landing/Landing.jsx
@@ -1,34 +1,53 @@
-import './landing.css';
-import { Background, HeartArrow, PopLines } from './import';
+import "./landing.css";
+import { Background, HeartArrow, PopLines } from "./import";
 
 const Landing = () => {
   return (
-    <div className="Background bg-green bg-no-repeat bg-bottom"  style={{ backgroundImage: `url(${Background})` }}>
-
+    <div
+      className="Background bg-green bg-no-repeat bg-bottom"
+      style={{ backgroundImage: `url(${Background})` }}
+    >
       <div className="text-center py-10 md:py-20">
-
         <div className="flex justify-center">
           <div className="-ml-2 -mt-8 w-14 h-14"></div>
-          <div className="Header text-4xl md:text-6xl font-space bg-no-repeat">How Lovable</div>
-          <img className="-ml-2 -mt-6 md:-mt-8 w-10 h-10 md:w-14 md:h-14" src={HeartArrow} alt="" />
+          <div className="Header text-4xl md:text-6xl font-space bg-no-repeat tracking-tight">
+            How Lovable
+          </div>
+          <img
+            className="-ml-2 -mt-6 md:-mt-8 w-10 h-10 md:w-14 md:h-14"
+            src={HeartArrow}
+            alt=""
+          />
         </div>
-        
-        <div className="Header text-4xl md:text-6xl font-space">Software is Built</div>
-        <div className="Subheader text-base md:text-xl py-3 font-open">Design, build, share what makes you smile</div>
 
-        <div className='flex'>
-          <div className='flex justify-center group mx-auto'>
-            <img className="w-10 h-10 mt-10 group-hover:scale-125 group-hover:translate-y-1 group-hover:-translate-x-1 duration-200" src={PopLines} alt="" />
-            <a href="https://www.figma.com/file/yUf60Lk7wxET028cQ6ukCS/Untitled?type=design&node-id=0%3A1&mode=design&t=9cSjTM2GTZo9Gxq0-1" target="_blank" rel="noopener noreferrer">
-              <button className="Button py-2 px-8 bg-orange group-hover:bg-purple duration-300 text-lg md:text-2xl font-bold dropbox font-space self-start">Join The Community</button>
+        <div className="Header text-4xl md:text-6xl font-space tracking-tight">
+          Software is Built
+        </div>
+        <div className="Subheader text-base md:text-xl py-3 font-open tracking-tight">
+          Design, build and share what makes you smile
+        </div>
+
+        <div className="flex">
+          <div className="flex justify-center group mx-auto">
+            <img
+              className="w-10 h-10 mt-10 group-hover:scale-125 group-hover:translate-y-1 group-hover:-translate-x-1 duration-200"
+              src={PopLines}
+              alt=""
+            />
+            <a
+              href="https://www.figma.com/file/yUf60Lk7wxET028cQ6ukCS/Untitled?type=design&node-id=0%3A1&mode=design&t=9cSjTM2GTZo9Gxq0-1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="Button py-2 px-8 bg-orange group-hover:bg-purple duration-300 text-lg md:text-2xl font-bold dropbox font-space self-start">
+                Join The Community
+              </button>
             </a>
             <div className="w-10 h-10 mt-10"></div>
           </div>
         </div>
-
       </div>
-        
     </div>
-  )
-}
-export default Landing
+  );
+};
+export default Landing;
--- my-project/src/components/product/Product.jsx
+++ my-project/src/components/product/Product.jsx
@@ -1,33 +1,73 @@
-import './product.css';
-import {  Data, Mobile } from './import';
+import "./product.css";
+import { Data, Mobile } from "./import";
 
 const Product = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-12 px-4 md:px-20 xl:px-60 py-10 md:py-20'>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-12 px-4 md:px-20 xl:px-60 py-10 md:py-20">
       <div className="box bg-green dropbox rounded-2xl p-5">
-        <div className="header font-space text-4xl">Design UX/UI with a personality</div>
-        <div className="paragraph py-2 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div>
-        <button className='bg-orange hover:bg-purple duration-300 font-space dropbox my-2 px-4 py-2 text-2xl'>Learn More</button>
+        <div className="header font-space text-4xl">
+          Design UX/UI with a personality
+        </div>
+        <div className="paragraph pb-2 pt-4 leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam.
+        </div>
+        <button className="bg-orange hover:bg-purple duration-300 font-space dropbox my-2 px-4 py-2 text-2xl">
+          Learn More
+        </button>
       </div>
 
       <div className="box bg-green dropbox rounded-2xl p-5">
-        <div className="header font-space text-4xl">Design UX/UI with a personality</div>
-        <div className="paragraph py-2 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div>
-        <button className='bg-orange hover:bg-purple duration-300 font-space dropbox my-2 px-4 py-2 text-2xl'>Learn More</button>
+        <div className="header font-space text-4xl">
+          Design UX/UI with a personality
+        </div>
+        <div className="paragraph pb-2 pt-4 leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam.
+        </div>
+        <button className="bg-orange hover:bg-purple duration-300 font-space dropbox my-2 px-4 py-2 text-2xl">
+          Learn More
+        </button>
       </div>
 
       <div className="box bg-green dropbox rounded-2xl p-5">
-        <div className="header font-space text-4xl">Design UX/UI with a personality</div>
-        <div className="paragraph py-2 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div>
-        <button className='bg-orange hover:bg-purple duration-300 font-space dropbox my-2 px-4 py-2 text-2xl'>Learn More</button>
+        <div className="header font-space text-4xl">
+          Design UX/UI with a personality
+        </div>
+        <div className="paragraph pb-2 pt-4 leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam.
+        </div>
+        <button className="bg-orange hover:bg-purple duration-300 font-space dropbox my-2 px-4 py-2 text-2xl">
+          Learn More
+        </button>
       </div>
 
       <div className="box bg-green dropbox rounded-2xl p-5">
-        <div className="header font-space text-4xl">Design UX/UI with a personality</div>
-        <div className="paragraph py-2 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div>
-        <button className='bg-orange hover:bg-purple duration-300 font-space dropbox my-2 px-4 py-2 text-2xl'>Learn More</button>
+        <div className="header font-space text-4xl">
+          Design UX/UI with a personality
+        </div>
+        <div className="paragraph pb-2 pt-4 leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam.
+        </div>
+        <button className="bg-orange hover:bg-purple duration-300 font-space dropbox my-2 px-4 py-2 text-2xl">
+          Learn More
+        </button>
       </div>
     </div>
-  )
-}
-export default Product
+  );
+};
+export default Product;

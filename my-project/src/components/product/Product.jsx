5a8d6279c29145298ccdfcbb8e2621e1d8233df2
George Claridge <glgclaridge@gmail.com>
2024-05-08 22:50:51 +1200

    Product Order

--- my-project/src/components/product/Product.jsx
+++ my-project/src/components/product/Product.jsx
@@ -4,7 +4,7 @@ import { Data, Build } from "./import";
 const Product = () => {
   return (
     <div className="grid items-center grid-cols-1 md:grid-cols-2 gap-12 px-4 md:px-20 xl:px-60 2xl:px-96 py-10 md:py-20">
-      <div className="box bg-green dropbox rounded-2xl p-5 h-min">
+      <div className="box bg-green dropbox rounded-2xl p-5 h-min md:order-1">
         <div className="header font-space text-4xl">
           Design UX/UI with a personality
         </div>
@@ -20,7 +20,7 @@ const Product = () => {
         </button>
       </div>
 
-      <div className="card dropbox">
+      <div className="card dropbox md:order-2">
 
         <div className="flex justify-between bg-orange py-2 px-4 border-b-[3px] border-black">
           <div className="header font-space">Get In The Flow With Wubble</div>
@@ -32,19 +32,19 @@ const Product = () => {
 
       </div>
 
-      <div className="card dropbox">
+      <div className="card dropbox order-last md:order-3">
 
         <div className="flex justify-between bg-purple py-2 px-4 border-b-[3px] border-black">
           <div className="header font-space">We Understand How Important Data Is</div>
         </div>
 
-        <div className="paragraph p-4 bg-white">
+        <div className="paragraph p-4 bg-white flex justify-center items-center">
           <img className='' src={Data} alt="" />
         </div>
 
       </div>
 
-      <div className="box bg-green dropbox rounded-2xl p-5 h-min">
+      <div className="box bg-green dropbox rounded-2xl p-5 h-min md:order-4">
         <div className="header font-space text-4xl">
           Design UX/UI with a personality
         </div>
